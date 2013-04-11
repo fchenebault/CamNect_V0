1cda05787720485d8f297e46575115eaf912e67e
Florent Chenebault <florent.chenebault@gmail.com>
2013-04-11 19:41:07 +0200

    Interface Start

---
+++ CamNect.GUI/CamNect.GUI/Views/Start.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CamNect.GUI.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Start.xaml
+    /// </summary>
+    public partial class Start : Window
+    {
+
+
+
+        public Start()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Window_Loaded(Object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
