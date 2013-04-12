f4d85cfab2c2031eb0d98679d3fa562caea26912
Florent Chenebault <florent.chenebault@gmail.com>
2013-04-12 13:28:36 +0200

    Changement interface Start

--- CamNect.GUI/CamNect.GUI/Views/Start.xaml.cs
+++ CamNect.GUI/CamNect.GUI/Views/Start.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CamNect.Kinect;
+using System;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace CamNect.GUI.Views
 {
@@ -10,26 +12,31 @@ namespace CamNect.GUI.Views
     /// </summary>
     public partial class Start : Window
     {
-
+        /* Variables */
+        private KinectMain kinect;
 
 
         public Start()
         {
-            InitializeComponent();
-
+            // Sensor initialisation
+            this.kinect = new KinectMain(sensorChooserUi, kinectRegion);
         }
 
         private void Window_Loaded(Object sender, RoutedEventArgs e)
         {
+            // Timer to wait for the other view
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(3000);
+            dispatcherTimer.Start();
         }
 
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-
+            //Views.Menu MenuPage = new Menu(kinect.sensorChooser);
+            //this.Content = MenuPage;
         }
-
-
 
     }
 }
